5a01f2d12228fd4e664ed10ff96b512d353f5b73
Jochem Hoes <r0666420@kuleuven.be>
2020-07-10 13:04:51 +0200

    start-server.sh without user prompts
    Removed createsuperuser from start-server
    changed populate to always select english list
    perform collecstatic without confirmation

--- start-server.sh
+++ start-server.sh
@@ -3,7 +3,7 @@
 # Prepare
 python manage.py makemigrations axes audit jet dashboard
 python manage.py migrate
-python manage.py createsuperuser
+#python manage.py createsuperuser
 python manage.py populate
-python manage.py collectstatic
+python manage.py collectstatic --no-input
 python manage.py runserver 0.0.0.0:$DJANGO_PORT
